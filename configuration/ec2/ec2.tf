32b7a2380d810d9d2764ab454d8e5f3d2f824002
devil730 <a30sham.07@gmail.com>
2022-09-12 18:23:15 +0530

    terraform upgradation

---
+++ configuration/ec2/ec2.tf
@@ -0,0 +1,7 @@
+module "my_ec2" {
+  source        = "../../modules/ec2"
+  ec2_count     = 1
+  ami_id        = "ami-5a8da735"
+  instance_type = "t2.micro"
+  subnet_id     = "${module.my_vpc.subnet_id}"
+}
